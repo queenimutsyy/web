22fac3071711449c55a1c7190814b3e229042895
queencc <meybesomedayagain@gmail.com>
2026-04-24 11:22:46 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,18 @@
 const databaseProyek = {
-    "sistem_post": {
+    "1": {
         judul: "siswa POS untuk PT ABC",
         keahlian: ["JavaScript", "PHP"],
-        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkd5wjv0O30JV1FpbqC2H8gqKXTzZHSuZzTA&s",
-        latar: "ini adalah projek apa aja",
+        gambar: "https://static.vecteezy.com/system/resources/thumbnails/008/997/535/small/abc-logo-abc-letter-abc-letter-logo-design-intitials-abc-logo-linked-with-circle-and-uppercase-monogram-logo-abc-typography-for-technology-business-and-real-estate-brand-vector.jpg",
+        latar: "This is my project",
+        tantangan: "",
+        pelajaran: "",
+        link: ""
+    },
+    "2": {
+        judul: "siswa POS untuk PT ABC",
+        keahlian: ["JavaScript", "PHP"],
+        gambar: "https://static.vecteezy.com/system/resources/thumbnails/008/997/535/small/abc-logo-abc-letter-abc-letter-logo-design-intitials-abc-logo-linked-with-circle-and-uppercase-monogram-logo-abc-typography-for-technology-business-and-real-estate-brand-vector.jpg",
+        latar: "This is my project",
         tantangan: "",
         pelajaran: "",
         link: ""
