6e20b2df4d19b961d73511ddeb521a40ea67f6a9
queencc <meybesomedayagain@gmail.com>
2026-04-24 09:22:33 +0700

    upload github pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_post": {
+        judul: "siswa POS untuk PT ABC",
+        keahlian: ["JavaScript", "PHP"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkd5wjv0O30JV1FpbqC2H8gqKXTzZHSuZzTA&s",
+        latar: "ini adalah projek apa aja",
+        tantangan: "",
+        pelajaran: "",
+        link: ""
+    },
+};
