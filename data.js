61ff678f9743dc19e25edcd23b4035660e189471
queencc <meybesomedayagain@gmail.com>
2026-04-24 12:00:59 +0700

    Update data.js

--- data.js
+++ data.js
@@ -9,9 +9,9 @@ const databaseProyek = {
         link: ""
     },
     "2": {
-        judul: "siswa POS untuk PHP ",
+        judul: "siswa POS untuk PT ABC ",
         keahlian: ["JavaScript", "PHP"],
-        gambar: "https://tamanubud.sgp1.cdn.digitaloceanspaces.com/gambar/81458845496-77723555319-bd2_6f5_636_330-1-original.jpg",
+        gambar: "https://static.vecteezy.com/system/resources/thumbnails/008/997/535/small/abc-logo-abc-letter-abc-letter-logo-design-intitials-abc-logo-linked-with-circle-and-uppercase-monogram-logo-abc-typography-for-technology-business-and-real-estate-brand-vector.jpg",
         latar: "This is my project",
         tantangan: "",
         pelajaran: "",
